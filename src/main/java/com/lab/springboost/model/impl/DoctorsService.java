5f3b5fbe6704da2aa5b58cd96b965ea95eb8b4c3
VLKO <konevskiyvladislav@gmail.com>
2019-06-06 13:56:35 +0400

    laba-3

---
+++ src/main/java/com/lab/springboost/model/impl/DoctorsService.java
@@ -0,0 +1,52 @@
+package com.lab.springboost.model.impl;
+
+import com.google.common.collect.Lists;
+import com.lab.springboost.model.DoctorsDAO;
+import com.lab.springboost.repository.DoctorsRepository;
+import com.lab.springboost.repository.PrescriptionsRepository;
+import com.lab.springboost.entity.DoctorEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class DoctorsService implements DoctorsDAO {
+    @Autowired
+    private DoctorsRepository doctorsRepository;
+
+    @Autowired
+    private PrescriptionsRepository prescriptionsRepository;
+
+    @Override
+    public void addDoctor(DoctorEntity doctor) {
+        doctorsRepository.save(doctor);
+    }
+
+    @Override
+    public void removeDoctor(DoctorEntity doctor) {
+        doctorsRepository.delete(doctor);
+    }
+
+    @Override
+    public void editDoctor(DoctorEntity doctor) {
+        doctorsRepository.save(doctor);
+    }
+
+    @Override
+    public DoctorEntity findById(Integer id) {
+        return doctorsRepository.findById(id);
+    }
+
+    @Override
+    public Integer prescriptionOfDoctor(Integer id) {
+        return prescriptionsRepository.countByDoctorByIddoctor_Id(id);
+    }
+
+    @Override
+    public List<DoctorEntity> allDoctors() {
+        return Lists.newArrayList(doctorsRepository.findAll());
+    }
+}
---
+++ src/main/java/com/lab/springboost/model/impl/PatientsService.java
@@ -0,0 +1,43 @@
+package com.lab.springboost.model.impl;
+
+import com.google.common.collect.Lists;
+import com.lab.springboost.model.PatientsDAO;
+import com.lab.springboost.repository.PatientsRepository;
+import com.lab.springboost.entity.PatientEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class PatientsService implements PatientsDAO {
+    @Autowired
+    private PatientsRepository patientsRepository;
+
+    @Override
+    public void addPatient(PatientEntity patient) {
+        patientsRepository.save(patient);
+    }
+
+    @Override
+    public void removePatient(PatientEntity patient) {
+        patientsRepository.delete(patient);
+    }
+
+    @Override
+    public void editPatient(PatientEntity patient) {
+        patientsRepository.save(patient);
+    }
+
+    @Override
+    public PatientEntity findById(Integer id) {
+        return  patientsRepository.findById(id);
+    }
+
+    @Override
+    public List<PatientEntity> allPatients() {
+        return Lists.newArrayList(patientsRepository.findAll());
+    }
+}
